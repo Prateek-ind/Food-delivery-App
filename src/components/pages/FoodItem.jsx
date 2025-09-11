41cc9d4454ada691a6f49e1297c7ae7f2bbb45cd
Prateek <prateek.kumar.ind@gmail.com>
2025-09-11 13:07:35 +0530

    fix id issue

--- src/components/pages/FoodItem.jsx
+++ src/components/pages/FoodItem.jsx
@@ -2,38 +2,36 @@ import React, { useContext, useState } from "react";
 import { assets } from "../../../assets/frontend_assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
-const FoodItem = ({ name, id, image, price, description, category }) => {
-  const { cartItems, addToCart, removeFromCart  } = useContext(StoreContext);
+const FoodItem = ({ name, _id, image, price, description, category }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   const formattedUSD = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   });
 
- 
-
   return (
     <div className="max-w-[250px] rounded-xl shadow-xl border border-gray-200 relative">
       <img className=" rounded-t-xl shadow-lg " src={image} alt="" />
-      {!cartItems[id] ? (
+      {!cartItems[_id] ? (
         <img
           className="max-w-11 rounded-full shadow-xl absolute right-2 bottom-48"
           src={assets.add_icon_white}
-          onClick={() => addToCart(id)}
+          onClick={() => addToCart(_id)}
         />
       ) : (
         <div className="bg-white p-1 rounded-full flex items-center gap-4 absolute right-2 bottom-48">
           <img
             className="rounded-full shadow-xl"
             src={assets.remove_icon_red}
-            onClick={() => removeFromCart (id)}
+            onClick={() => removeFromCart(_id)}
             alt=""
           />
-          <p className="text-lg font-semibold ">{cartItems[id]}</p>
+          <p className="text-lg font-semibold ">{cartItems[_id]}</p>
           <img
             className="rounded-full shadow-xl"
             src={assets.add_icon_green}
-            onClick={() => addToCart(id)}
+            onClick={() => addToCart(_id)}
             alt=""
           />
         </div>
