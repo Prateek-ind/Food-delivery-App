2fa190ddffd306c6e28ec7c41207b5ccb13bfdc5
Prateek <prateek.kumar.ind@gmail.com>
2025-09-11 11:10:28 +0530

    fix input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Input = ({ label, type, ...props }) => {
-  return <div className="flex">
-    <label htmlFor={label} className="text-gray-500">{label}</label>
-    <input name={label} type={type} {...props} />
-  </div>
+const Input = ({ name, type, ...props }) => {
+  return (
+    <div className="flex flex-col gap-1 w-full">
+      <label htmlFor={name} className="text-gray-800">
+        {name}
+      </label>
+      <input name={name} type={type} {...props} className="border border-gray-500 outline-none rounded-xl px-4 py-2 " />
+    </div>
+  );
 };
 
 export default Input;
