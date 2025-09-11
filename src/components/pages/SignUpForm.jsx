e50e849eecb985320d62cbdc25f4bc25ab73e86e
Prateek <prateek.kumar.ind@gmail.com>
2025-09-11 11:10:02 +0530

    fix sign up form

--- src/components/pages/SignUpForm.jsx
+++ src/components/pages/SignUpForm.jsx
@@ -1,19 +1,64 @@
 import React, { forwardRef } from "react";
+import Input from "../Input";
+import  Button  from "../Button";
 
-const SignUpForm = forwardRef((props, ref) => {
+const SignUpForm = forwardRef(({ setIsLoggedIn }, ref) => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form Submitted");
+  }
+
   return (
-    <form ref={ref} className="flex flex-col gap-4">
+    <form
+      ref={ref}
+      onSubmit={handleSubmit}
+      className="w-full flex flex-col gap-4"
+    >
       <h2 className="text-2xl text-center font-semibold">Sign-Up</h2>
-      <div className="flex justify-between">
-        <Input name="First Name" type="text" />
-        <Input name="Last Name" type="text" />
+      <div className="flex gap-4 justify-between">
+        <Input
+          name="First Name"
+          type="text"
+          placeholder="Enter your First Name"
+        />
+        <Input
+          name="Last Name"
+          type="text"
+          placeholder="Enter your Last Name"
+        />
       </div>
-      <Input name="Email" type="email" />
-      <div className="flex justify-between">
-        <Input name="Password" type="password" />
-        <Input name="Confirm-Password" type="password" />
+      <Input name="Email" type="email" placeholder="Enter your Email" />
+      <div className="flex gap-4 justify-between">
+        <Input
+          name="Password"
+          type="password"
+          placeholder="Enter your password"
+        />
+        <Input
+          name="Confirm-Password"
+          type="password"
+          placeholder="Confirm Password"
+        />
       </div>
-      <Button type="submit" name="Sign-Up" />
+      <Button
+        type="submit"
+        name="Log-In"
+        className="w-fit rounded-xl mt-2 text-white px-4 py-2 bg-orange-500 hover:bg-orange-700 hover:scale-103 cursor-pointer"
+      >
+        Sign Up
+      </Button>
+      <p className="py-2 text-gray-700 text-sm">
+        If you are already registered please  
+        {
+          <a
+            className="text-orange-500 hover:font-bold cursor-pointer"
+            onClick={() => setIsLoggedIn(true)}
+          >
+             Sign In
+          </a>
+        }{" "}
+        here
+      </p>
     </form>
   );
 });
