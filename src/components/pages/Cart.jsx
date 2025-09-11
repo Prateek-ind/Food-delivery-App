e6d3c434a8ebd6040a36a1681f6f1dc0eceffb96
Prateek <prateek.kumar.ind@gmail.com>
2025-09-11 13:07:20 +0530

    add cart component and route

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="pt-20 mt-8">
+      <div className="">
+        <div className="grid grid-cols-6 items-center justify-center text-center py-2">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, i) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={i} className="grid grid-cols-6 items-center justify-center text-center">
+                <img src={item.image} className="w-24 p-4 " alt="" />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>{item.price*(cartItems[item._id])}</p>
+                <button onClick={()=>removeFromCart(item._id)}>X</button>
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
