8e4b2fd00ccb6f8059ba75fd032f1514b555b34b
Prateek <prateek.kumar.ind@gmail.com>
2025-09-14 11:45:12 +0530

    add fallback text to cart when empty

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -11,43 +11,53 @@ const Cart = () => {
 
   return (
     <div className="pt-20 mt-24 max-w-7xl px-8 flex items-center justify-center">
-      <div className="">
-        <div className="grid grid-cols-6 items-center justify-center text-center font-semibold py-2">
-          <p>Items</p>
-          <p>Title</p>
-          <p>Price</p>
-          <p>Quantity</p>
-          <p>Total</p>
-          <p>Remove</p>
-        </div>
-        <br />
-        <hr />
-        {food_list.map((item, i) => {
-          if (cartItems[item._id] > 0) {
-            return (
-              <div className="">
-                <div
-                  key={i}
-                  className="grid grid-cols-6 items-center justify-center text-center"
-                >
-                  <img src={item.image} className="w-24 p-2 mx-auto " alt="" />
-                  <p>{item.name}</p>
-                  <p>{formattedUSD.format(item.price)}</p>
-                  <p>{cartItems[item._id]}</p>
-                  <p>{formattedUSD.format(item.price * cartItems[item._id])}</p>
-                  <button
-                    onClick={() => removeFromCart(item._id)}
-                    className="font-bold text-orange-500 hover:scale-105 cursor-pointer"
+      {Object.values(cartItems) > 0 ? (
+        <div className="">
+          <div className="grid grid-cols-6 items-center gap-4 justify-center text-center font-semibold py-2">
+            <p>Items</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
+          </div>
+          <br />
+          <hr />
+          {food_list.map((item, i) => {
+            if (cartItems[item._id] > 0) {
+              return (
+                <div className="">
+                  <div
+                    key={i}
+                    className="grid grid-cols-6 gap-4 items-center justify-center text-center"
                   >
-                    X
-                  </button>
+                    <img
+                      src={item.image}
+                      className="w-24 p-2 mx-auto "
+                      alt=""
+                    />
+                    <p>{item.name}</p>
+                    <p>{formattedUSD.format(item.price)}</p>
+                    <p>{cartItems[item._id]}</p>
+                    <p>
+                      {formattedUSD.format(item.price * cartItems[item._id])}
+                    </p>
+                    <button
+                      onClick={() => removeFromCart(item._id)}
+                      className="font-bold text-orange-500 hover:scale-105 cursor-pointer"
+                    >
+                      X
+                    </button>
+                  </div>
+                  <hr className="text-gray-400" />
                 </div>
-                <hr className="text-gray-400" />
-              </div>
-            );
-          }
-        })}
-      </div>
+              );
+            }
+          })}
+        </div>
+      ) : (
+        <p>Your cart is empty please add items to checkout.</p>
+      )}
     </div>
   );
 };
