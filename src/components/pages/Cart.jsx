d2d286e040c08c4c226be0b9932b3f3c3fee347c
Prateek <prateek.kumar.ind@gmail.com>
2025-09-14 11:26:11 +0530

    fix cart style

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -10,9 +10,9 @@ const Cart = () => {
   });
 
   return (
-    <div className="pt-20 mt-8">
+    <div className="pt-20 mt-24 max-w-7xl px-8 flex items-center justify-center">
       <div className="">
-        <div className="grid grid-cols-6 items-center justify-center text-center py-2">
+        <div className="grid grid-cols-6 items-center justify-center text-center font-semibold py-2">
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
@@ -25,21 +25,24 @@ const Cart = () => {
         {food_list.map((item, i) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div
-                key={i}
-                className="grid grid-cols-6 items-center justify-center text-center"
-              >
-                <img src={item.image} className="w-24 p-2 mx-auto " alt="" />
-                <p>{item.name}</p>
-                <p>{formattedUSD.format(item.price)}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{formattedUSD.format(item.price * cartItems[item._id])}</p>
-                <button
-                  onClick={() => removeFromCart(item._id)}
-                  className="font-bold text-orange-500 hover:scale-105 cursor-pointer"
+              <div className="">
+                <div
+                  key={i}
+                  className="grid grid-cols-6 items-center justify-center text-center"
                 >
-                  X
-                </button>
+                  <img src={item.image} className="w-24 p-2 mx-auto " alt="" />
+                  <p>{item.name}</p>
+                  <p>{formattedUSD.format(item.price)}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{formattedUSD.format(item.price * cartItems[item._id])}</p>
+                  <button
+                    onClick={() => removeFromCart(item._id)}
+                    className="font-bold text-orange-500 hover:scale-105 cursor-pointer"
+                  >
+                    X
+                  </button>
+                </div>
+                <hr className="text-gray-400" />
               </div>
             );
           }
