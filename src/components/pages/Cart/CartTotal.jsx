3510140aafb4e83ca5fd412ab3646fbc770a28cd
Prateek <prateek.kumar.ind@gmail.com>
2025-09-15 13:50:54 +0530

    fix cart total structure

--- src/components/pages/Cart/CartTotal.jsx
+++ src/components/pages/Cart/CartTotal.jsx
@@ -4,7 +4,7 @@ import Button from "../../Button";
 
 import { useNavigate } from "react-router-dom";
 
-const CartTotal = () => {
+const CartTotal = ({proceedTo}) => {
   const { getCartTotal } =
     useContext(StoreContext);
 
@@ -19,7 +19,7 @@ const CartTotal = () => {
   const deliveryFee = 2;
   const total = subtotal + deliveryFee;
   return (
-    <div className="w-full py-8 mt-4 grid grid-cols-1 sm:grid-cols-2 gap-16 justify-between">
+    
       <div className="">
         <h3 className="text-xl font-bold py-2">Cart Total</h3>
         <div className="flex items-center py-2 justify-between">
@@ -41,10 +41,10 @@ const CartTotal = () => {
           className="text-white bg-orange-500 px-4 py-2 rounded-lg shadow cursor-pointer
                hover:scale-105 hover:bg-orange-700 flex items-center justify-center"
         >
-          PROCEED TO CHECKOUT
+          {proceedTo}
         </Button>
       </div>
-    </div>
+    
   );
 };
 
